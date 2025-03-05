8f49039cd5a724b4e34a752b37d047be8bbd9d28
Ben Armstrong <armstrongben.1a@gmail.com>
2025-03-05 21:51:36 +0000

    Added number input and verification

--- Sqrt/Program.cs
+++ Sqrt/Program.cs
@@ -1,7 +1,19 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-static unsafe void Main(string[] args)
+class Program
 {
-
+    static unsafe void Main(string[] args)
+    {
+        double n = 0;
+        while (n == 0)
+        {
+            Console.WriteLine("Please enter a number to be square rooted (other than 0):");
+            var temp = Console.ReadLine();
+            double.TryParse(temp, out n);
+            Console.Clear();
+        }
+        Console.WriteLine(n.ToString());
+    }
 }
+
